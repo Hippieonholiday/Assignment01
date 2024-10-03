a34fd967f345c0b601436db6771c0b4e40357df8
Hippieonholiday <cassidybrynn@gmail.com>
2024-10-02 20:44:41 -0800

    first

---
+++ js/script.js
@@ -0,0 +1,132 @@
+// STEP 1
+/*let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction; */
+
+// STEP 2
+/*let number = 5;
+let greeting = 'hello';
+let question = false;
+let anExpression = null;*/
+
+// STEP 3
+/*let grade = 2 + 7;
+let myage*/;
+
+// STEP 4
+/*let firstName;
+let lastName;
+let address;
+let city;
+let state;
+let intZipCode;
+let intYourAge;
+let strReferralSource;
+let strMayWeContactYou;*/
+
+// STEP 5
+/*let firtName
+    firstName = 'Cassidy';
+let intZipCode = 92115;
+let intYourAge = year - 1998, year = 2024;*/
+
+// STEP 6
+/*let num = 16;
+let str = "Days until deadline";
+let result = num + str;
+alert(result)​*/
+
+// STEP 7
+/*let bln = true;
+let str = "You got it right!";
+let result = bln + str;
+alert(result)
+let num = 19;
+let bln2 = "false";
+let result2 = num + bln2;
+alert(result2)*/
+
+// STEP 8
+/*let someString = (Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.")
+console.log(someString)*/
+
+// STEP 9
+/*let zero = null 
+    alert(zero);
+let x
+    alert(x);*/
+
+// STEP 10
+/*typeof testing;
+typeof 5;
+typeof true;
+let house = new Object()
+typeof house;
+let x
+typeof x;*/
+
+// STEP 11
+/*let x = 'Hello Cassidy Stokes,'
+let y = 'welcome to the JavaScript class!'
+alert (x+y);*/
+
+// STEP 12
+/*let firstName = 'Cassidy'
+let y = 'welcome to the JavaScript class!'
+alert ('hello'+firstName+y);*/
+
+// STEP 13
+/*let course = 'javascript';
+let firstName = 'Cassidy';
+let y = 'welcome to the';
+alert ('hello'+firstName+y+course+'class!');*/
+
+// STEP 14
+/*let course = 'javascript';
+let firstName = 'Cassidy';
+let y = 'welcome to the';
+alert ('hello'+firstName/n+y+course+'class!');*/
+
+// STEP 15
+/*let course = 'javascript';
+let firstName = prompt('what is your name?');
+let y = 'welcome to the';
+alert ('hello'+firstName/n+y+course+'class!');*/
+
+// STEP 16
+/*let course = prompt('what class are you in?');
+let firstName = prompt('what is your name?');
+let y = 'welcome to the';
+alert ('hello'+firstName/n+y+course+'class!');*/
+
+// STEP 17
+/*let x = 10
+let y = 20
+console.log(x + y);*/
+
+// STEP 18
+/*let x = 20
+x += 20
+console.log(x);*/
+
+// STEP 19
+/*let x = 20
+x *= 5
+console.log(x);*/
+
+// STEP 20
+/*let x = (20 / 3)
+x /= 1
+console.log(x);*/
+
+// STEP 21
+/*let x = 5
+let y = 10
+alert(x == 5 && y == 10);*/
+
+// STEP 22
+/*let x = 6
+let y = 12
+alert(y < x && x == 6);*/
